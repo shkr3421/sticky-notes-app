ee00c11297dc9ef5f700c7ff074cd446ad5bcaae
shkr3421 <shkr9886@gmail.com>
2026-04-14 21:22:47 +0530

    fix: datasource + dialect + fall

--- src/main/java/com/example/stickyNotesAPI/StickyNotesApiApplication.java
+++ src/main/java/com/example/stickyNotesAPI/StickyNotesApiApplication.java
@@ -7,7 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StickyNotesApiApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("DB_URL = " + System.getenv("DB_URL"));
+		System.out.println("DATABASE_URL = " + System.getenv("DATABASE_URL"));
+
 		SpringApplication.run(StickyNotesApiApplication.class, args);
+
+
 	}
 
 }
